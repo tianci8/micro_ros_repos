0b2f1c923f6a57a59f61f6bd3e9eb013c71fa6d9
Tianci <979308702@qq.com>
2022-07-28 13:30:20 +0800

    第一次提交

---
+++ best_effort_subscription/src/sub_best_effort/src/sub_best_effort.cpp
@@ -0,0 +1,32 @@
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+
+using std::placeholders::_1;
+
+class MinimalSubscriber : public rclcpp::Node
+{
+public:
+  MinimalSubscriber()
+  : Node("minimal_subscriber")
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::Int32>("micro_ros_arduino_node_publisher", rclcpp::SensorDataQoS(), std::bind(&MinimalSubscriber::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const std_msgs::msg::Int32::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "The int32 data is [%d] ", msg->data);
+  }
+  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
