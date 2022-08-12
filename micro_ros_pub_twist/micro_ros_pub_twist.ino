b3cacaa99a8fc412d546df4c32167f9ad3075c79
Tianci <979308702@qq.com>
2022-08-12 16:48:36 +0800

    发布twsit

---
+++ micro_ros_pub_twist/micro_ros_pub_twist.ino
@@ -0,0 +1,94 @@
+#include <micro_ros_arduino.h>
+
+#include <stdio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <geometry_msgs/msg/twist.h>  //changed!
+
+rcl_publisher_t publisher;
+geometry_msgs__msg__Twist msg; //changed!-->modify msg type <twist__struct.h>
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
+rcl_timer_t timer;
+
+#define LED_PIN 27 //changed!-->Modify M5 stack Atom Lite LED pin
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+
+
+void error_loop(){
+  while(1){
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    delay(100);
+  }
+}
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{  
+  RCLC_UNUSED(last_call_time);
+  if (timer != NULL) {
+    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+    
+    static int cnt = 0;
+    msg.linear.x = 0.5;                            //const linear.x
+    msg.angular.z = 1.0 - 0.01*cnt;               //variable angular.z
+    cnt++;
+    
+  }
+}
+
+void setup() {
+  set_microros_transports();
+  
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);  
+  
+  delay(2000);
+
+  allocator = rcl_get_default_allocator();
+
+  //create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "micro_ros_arduino_node", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_default(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "turtle1/cmd_vel"));  //changed!-->modify topic name
+
+  // create timer,
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+  // create executor
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  // changed!-->msg initialization
+  msg.linear.x=0;
+  msg.linear.y=0;
+  msg.linear.z=0;
+  msg.angular.x=0;
+  msg.angular.y=0;
+  msg.angular.z=0;
+}
+
+void loop() {
+//  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+}
