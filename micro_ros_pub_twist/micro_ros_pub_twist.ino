ba386d8cf7bec412733868d38fe34d4926503d64
z2010012286 <979308702@qq.com>
2022-08-13 21:18:56 +0800

    发布twist

--- micro_ros_pub_twist/micro_ros_pub_twist.ino
+++ micro_ros_pub_twist/micro_ros_pub_twist.ino
@@ -21,8 +21,6 @@ rcl_timer_t timer;
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
-
-
 void error_loop(){
   while(1){
     digitalWrite(LED_PIN, !digitalRead(LED_PIN));
